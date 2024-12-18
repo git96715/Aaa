bc7f54c04bc6fb97101fcbd455357ccbc7c82b9d
git96715 <git.hubno5@gmail.com>
2024-12-18 19:09:17 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8119423303:AAFqAkYlyFQx3IZcc0vAZ4TgXYDjcgRld2w")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@LDX_COBRA")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5660031361")
+
+
+
+
+
+
+
+  
